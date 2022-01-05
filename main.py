00728da50721c65a633f986d552806fc497be669
Nikolay Andreychik <kolya.andreychik@gmail.com>
2022-01-05 14:34:37 +0300

    Reworked output data from DataLoader

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import numpy as np
 import torch
 from torch.utils.data import Dataset, DataLoader
 from sklearn.model_selection import train_test_split
+
+import matplotlib
 import matplotlib.pyplot as plt
+
+matplotlib.use('TkAgg')
 
 class CustomDataset(Dataset):
 	def __init__(self):
@@ -27,23 +31,42 @@ class CustomDataset(Dataset):
 
 	def __getitem__(self, idx):
 		img_path, class_name = self.data[idx]
-		img = cv2.imread(img_path)
-		# print(img.shape)
-		img = cv2.resize(img, self.img_dim)
-	    # сопоставление имени с числом
+
+		# сопоставление имени с числом
 		class_id = self.class_map[class_name]
-	    # преобразуем переменные в тензоры (torch.from_numpy позволяет
-	    # преобразовать массив numpy в тензор)
-		img_tensor = torch.from_numpy(img)
-	    # замены осей ((Каналы, Ширина, Высота))
-		img_tensor = img_tensor.permute(2, 0, 1)
+
 	    # преобразуем целочисленное значение class_id в тензор
 	    # также увеличиваем его размерность, ссылаясь на него как [class_id].
 	    # Это необходимо для того, чтобы обеспечить возможность пакетной
 	    # обработки данных в тех размерах, которые требуются torch.
 		class_id = torch.tensor([class_id])
 
-		return img_tensor.float(), class_id.float()
+		return img_path, class_id.float()
+
+	def getImgsTensors(self, imgs_path):
+		output_tensor = torch.tensor([], dtype=torch.float)
+
+		for img_path in imgs_path:
+			img = cv2.imread(img_path)
+			# print(img.shape)
+			img = cv2.resize(img, self.img_dim)
+
+		    # преобразуем переменные в тензоры (torch.from_numpy позволяет
+		    # преобразовать массив numpy в тензор)
+			img_tensor = torch.from_numpy(img)
+			# замены осей ((Каналы, Ширина, Высота))
+			img_tensor = img_tensor.permute(2, 0, 1).float()
+
+			img_tensor = img_tensor.unsqueeze(0)
+
+			output_tensor = torch.cat((output_tensor, img_tensor))
+
+		return output_tensor
+
+	def getName(self, value):
+	    for k, v in self.class_map.items():
+	        if v == value:
+	            return k
 
 if __name__ == "__main__":
 	# Чтобы протестировать набор данных и наш загрузчик данных, в главной
@@ -52,20 +75,20 @@ if __name__ == "__main__":
 	dataset = CustomDataset()
 	dataset_train, dataset_test = train_test_split(dataset, test_size=0.7)
 
-	# 	data_loader = DataLoader(dataset, batch_size=4, shuffle=True)
 	data_loader_train = DataLoader(dataset_train, batch_size=4, shuffle=True)
 	data_loader_test = DataLoader(dataset_test, batch_size=4, shuffle=True)
-	# 	for imgs, labels in data_loader_train:
-	#   		print("Batch of images has shape: ",imgs.shape)
-			# print("Batch of labels has shape: ", labels.shape)
+
 	# Display image and label.
 	train_features, train_labels = next(iter(data_loader_train))
+	train_features = dataset.getImgsTensors(train_features)
+
 	print(f"Feature batch shape: {train_features.size()}")
 	print(f"Labels batch shape: {train_labels.size()}")
 	img = train_features[0].squeeze()
 	label = train_labels[0]
-	plt.imshow(img, cmap="gray")
+
+	plt.title(f"Label: {dataset.getName(label)}")
+	plt.imshow(cv2.cvtColor(img.permute(1, 2, 0).numpy() / 255, cv2.COLOR_BGR2RGB))
 	plt.show()
-	print(f"Label: {label}")
 	# Провели первичную классификацию данных. Теперь нужно разделить данные
 	# на тестовые и тренировочные
